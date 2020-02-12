825d479a480f7a37c2df8dc4ea65c28be4ae2c2a
Olivier <olivier@macbook-air-de-olivier.home>
2020-02-12 19:23:02 +0100

    add erase function

--- js/function.js
+++ js/function.js
@@ -1,20 +1,23 @@
+let input = "";
 
 
-let input="";
-
-
-function displayValue(btn){
+function displayValue(btn) {
     input = input + btn.value;
-    document.getElementById("display").innerHTML= input;
+    document.getElementById("display").innerHTML = input;
 };
 
 
-function result(){
+function result() {
     document.getElementById("display").innerHTML = eval(input);
 };
 
 
-function reset(){
+function reset() {
     document.getElementById("display").innerHTML = "0";
-    input="";
+    input = "";
+};
+
+function erase(btn) {
+    input  = input.slice(0, input.length-1);
+    document.getElementById("display").innerHTML = input;
 };
